9cc94531fbad9ac805831d93a946aa47c30d6420
Tudor Duicu <tudorduicu@gmail.com>
2025-11-21 14:00:58 +0100

    changed tf name for dc

---
+++ infrastructure/platform/domain-controller.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "ad" {
+  name        = "innovatech-ad-sg"
+  description = "Allow RDP and LDAP from Home"
+  vpc_id      = aws_vpc.main.id
+
+  # Allow RDP (Remote Desktop) from your home
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow LDAP (Active Directory) from your home
+  ingress {
+    from_port   = 389
+    to_port     = 389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "domain_controller" {
+  ami           = "ami-032c954118df38cd0" 
+  instance_type = "t3.medium"
+  key_name      = aws_key_pair.deployer_key.key_name
+  
+  # Must be a PUBLIC subnet so you can reach it from home
+  subnet_id                   = aws_subnet.public["public_1"].id
+  vpc_security_group_ids      = [aws_security_group.ad.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Innovatech-DC"
+  }
+}
+
+output "dc_public_ip" {
+  value = aws_instance.domain_controller.public_ip
+}
